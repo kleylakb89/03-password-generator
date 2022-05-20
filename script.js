d96fc39a79258d55b9e8703b77bf55f7a516d9ee
Kristin Kleyla <clever_disguises@yahoo.com>
2022-05-20 19:51:11 -0400

    figuring out the random number generator

--- script.js
+++ script.js
@@ -3,10 +3,44 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  generatePassword = function() {
+
+    var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+    var char = prompt("How many characters would you like your password to contain?");
+
+    if (char < 8) {
+      alert("Password must be at least 8 characters.");
+    } else if (char > 129) {
+      alert("Password must be under 129 characters.");
+    }
+
+    // var spec = confirm("Would you like to include lowercase characters?");
+
+
+
+    var pass = Math.floor(Math.random()*10);
+    // var pass = "";
+    // for (i=0; i<26; i++) {
+    //   var newPass = pass.padEnd(char, alpha[i]);
+    //   console.log(newPass);
+    // }
+
+    // for (pass = ""; pass.length<char; pass.padEnd(char, alpha[Math.random()*26])) {
+
+    //   console.log(pass);
+    // }
+    return(pass);
+  }
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  
+
   passwordText.value = password;
+  console.log(password);
 
 }
 
