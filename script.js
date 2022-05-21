4ed486d2dcf46237254f29295b3a94d3dede74a7
Kristin Kleyla <clever_disguises@yahoo.com>
2022-05-20 20:19:48 -0400

    cleaned up code layout

--- script.js
+++ script.js
@@ -6,28 +6,18 @@ function writePassword() {
 
   generatePassword = function() {
 
+    var pass = "";
+
     var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
     var upAlpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
     var special = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
-    var char = prompt("How many characters would you like your password to contain?");
-
-    if (char < 8) {
-      alert("Password must be at least 8 characters.");
-    } else if (char > 129) {
-      alert("Password must be under 129 characters.");
-    }
-
-    // var spec = confirm("Would you like to include lowercase characters?");
-
-    var pass = "";
     numPass = function() {
       for (var i = 0; i<=char; i++){
         pass = pass.padEnd(i, Math.floor(Math.random()*10));
       }
-      // var num = Math.floor(Math.random()*10);
       return(pass);
     }
 
@@ -52,19 +42,18 @@ function writePassword() {
       return(pass);
     }
 
+    var char = prompt("How many characters would you like your password to contain?");
+
+    if (char < 8) {
+      alert("Password must be at least 8 characters.");
+    } else if (char > 129) {
+      alert("Password must be under 129 characters.");
+    }
+
+    // var spec = confirm("Would you like to include lowercase characters?");
+
     specialPass();
-    // numPass();
-    // var num = Math.floor(Math.random()*10);
-    // var pass = "";
-    // for (i=0; i<26; i++) {
-    //   var newPass = pass.padEnd(char, alpha[i]);
-    //   console.log(newPass);
-    // }
-
-    // for (pass = ""; pass.length<char; pass.padEnd(char, alpha[Math.random()*26])) {
-
-    //   console.log(pass);
-    // }
+    
     return(pass);
   }
 
